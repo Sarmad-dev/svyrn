2fce247481797cd5875d37a59b6e1f4cf21e0dc4
unknown <sarmadjkl@gmail.com>
2025-08-02 10:05:00 +0500

    removed suspense error

--- src/app/(main)/chat/page.tsx
+++ src/app/(main)/chat/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { Chat } from "@/components/chat/Chat";
 import Header from "@/components/layout/header/header";
-import React from "react";
+import React, { Suspense } from "react";
 
 const ChatPage = () => {
   return (
     <div className="w-full">
       <Header />
-      <Chat />
+      <Suspense fallback={<div>Loading Chats</div>}>
+        <Chat />
+      </Suspense>
     </div>
   );
 };
