1259bcf5513e2c98be46719f5d62b8a00f5ad194
unknown <sarmadjkl@gmail.com>
2025-08-02 11:05:45 +0500

    added middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const protectedRoutes = [
+  "/home",
+  "/ads",
+  "/groups",
+  "/marketplace",
+  "/pages",
+  "/post",
+  "/settings",
+  "profile",
+  "/user",
+  "/chat",
+];
+const authRoutes = ["/sign-in", "/sign-up", "/forgot-password"];
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("auth_token")?.value;
+  const pathname = request.nextUrl.pathname;
+
+  // Redirect unauthenticated user from protected pages
+  if (protectedRoutes.some((r) => pathname.startsWith(r)) && !token) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  // Redirect logged-in user away from auth pages
+  if (authRoutes.includes(pathname) && token) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
