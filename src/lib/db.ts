2ef3a5172101c80f647452b972b5d98e1b1d26b7
unknown <sarmadjkl@gmail.com>
2025-08-03 10:47:47 +0500

    adding mongo uri

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,13 @@
 // lib/mongodb.ts
 import { MongoClient } from "mongodb";
 
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI as string; // e.g., mongodb+srv://...
+const uri = process.env.MONGODB_URI as string; // e.g., mongodb+srv://...
 
-if (!uri) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
+// if (!uri) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env.local"
+//   );
+// }
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
