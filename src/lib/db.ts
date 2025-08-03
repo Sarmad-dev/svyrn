cc0324cccf35a988b66a93494049ddeec565a48b
unknown <sarmadjkl@gmail.com>
2025-08-03 10:32:16 +0500

    changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
-    throw new Error("MONGODB_URI is not defined");
-  }
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI, {
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI!, {
     dbName: "social-network",
   });
 
