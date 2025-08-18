feb01885c40609e86dc92e20a08b568afabd301a
unknown <sarmadjkl@gmail.com>
2025-08-18 21:28:09 +0500

    changes

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatLeftside from "./chat-leftside";
 import Chatarea from "./chat-area";
 import { useQueryState } from "nuqs";
 import ChatSheet from "./chat-sheet";
+import { useQuery } from "@tanstack/react-query";
+import { getConversation } from "@/lib/actions/conversation.action";
+import { authClient } from "@/lib/auth-client";
 
 export const Chat: React.FC = () => {
-  const [conversationId] = useQueryState("conversationId");
+  const { data } = authClient.useSession();
+  const [conversationId, setConversationId] = useQueryState("conversationId");
+
+  const { data: conversation } = useQuery({
+    queryKey: ["conversation", conversationId],
+    queryFn: async () =>
+      await getConversation({
+        token: data?.session.token as string,
+        id: conversationId as string,
+      }),
+    enabled: !!conversationId && !!data?.session.token,
+  });
+
+  useEffect(() => {
+    setConversationId(conversation?.data?._id);
+  }, [conversation?.data?._id]);
 
   return (
     <>
